7bdebd89bbba2a689b12db67a7403b5fdd45ef7f
Clemens Lee <operations.sg65@outlook.com>
2024-10-01 14:48:34 +0000

    JS renderlist commit

--- data.js
+++ data.js
@@ -14,3 +14,11 @@ let books = [{
     "isbn": 3
 }]
 
+function renderList () {
+    display.innerHTML = "";
+    for (let b of books) {
+        let liElement = document.createElement("li");
+        liElement.innerHTML = `Book Title: ${b.title}  Author: ${b.author}  ISBN: ${b.isbn} <input type="checkbox" class="checkbox"/><button id="edit">Edit</button><button id="delete">Delete</button>`;
+        display.append(liElement);
+    }
+}
--- script.js
+++ script.js
@@ -2,10 +2,6 @@
 
 let display = document.querySelector("#display");
 document.querySelector("#viewAllBooks").addEventListener("click",() => {
-    display.innerHTML = "";
-    for (let b of books) {
-        let liElement = document.createElement("li");
-        liElement.innerHTML = `Book Title: ${b.title}  Author: ${b.author}  ISBN: ${b.isbn}`;
-        display.append(liElement);
-    }
+    renderList();
+    
 })
